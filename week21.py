bc201148dfae215e983f2155142b7989c629275d
Amanda Matheu <amanda.matheu@gmail.com>
2021-07-28 09:48:02 -0500

    updated comments

--- week21.py
+++ week21.py
@@ -49,14 +49,15 @@ def add_data():
         with open(filenm,'r+') as file:
             # First we load existing data into a dict.
             file_data = json.load(file)
-            # Join new_data with file_data inside emp_details
+            # Join new_data with file_data inside prizes
             file_data["prizes"].append(data)
-            # Sets file's current position at offset.
+            # Sets file's current position at offset. not understanding this step
             file.seek(0)
             # convert back to json.
             json.dump(file_data, file, indent = 4)
         # I'd like to return a statement that it was added and the data
-        # but I don't know how do that
+        # but I don't know how do that, possibly a new html page
+        # way beyond my current capabilities
         return '<p>Data added </p>'
 
     else:    
