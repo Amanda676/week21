4be6a867d54e612860c6fefb9498a47c171085bb
Amanda Matheu <amanda.matheu@gmail.com>
2021-07-28 09:23:19 -0500

    cleaned up code

--- week21.py
+++ week21.py
@@ -4,7 +4,7 @@
 #pip install flask 
 #export FLASK_APP=flask-app
 #flask run
-from flask import Flask, json, render_template, request, jsonify, redirect, url_for
+from flask import Flask, json, render_template, request, jsonify
 
 import os
 
@@ -17,7 +17,7 @@ def home_page():
     welcome_msg = '<p>Nobel Data</p> \
     <p>/all for all years</p> \
     <p>/all/year for one year</p> \
-    <p>/add_data for input data function</p>'
+    <p>/add_data for input data function, returns data</p>'
     return welcome_msg
 
 @app.route("/all")
@@ -37,37 +37,13 @@ def get_year(year):
     #render_template is always looking in templates folder
     return render_template('index.html',data=output_data)
 
-#this doesn't work, commenting it out
-'''
-@app.route("/datainput", methods=['POST', "GET"])
-def datainput():
-    data = {'year': '2025', 'category': 'test_garbage', 'laureates': 
-    [{'id': 'test_1', 'firstname': 'Testing', 'surname': 'Lastname',
-     'motivation': '"testing purposes"', 
-     'share': '1'}]}
-    
-    json_url = os.path.join(app.static_folder,"","nobel.json")
-    #response = requests.post(json_url, json=data) 
-    #print(response.status_code) 
-    #print(response.text)
-    return jsonify(data)
-'''
 
 @app.route("/add_data", methods=["POST", "GET"])    
 def add_data():
     if request.method == "POST":
-        '''
-        year = request.form['year']
-        category = request.form['category']
-        n_id = request.form['id']
-        firstname = request.form['firstname']
-        surname = request.form['surname']
-        motivation = request.form['motivation']
-        share = request.form['share']
-        '''
-        
+        # data in a diction
         data = request.form
-        
+        # file path
         filenm = "./static/nobel.json"
 
         with open(filenm,'r+') as file:
@@ -79,26 +55,12 @@ def add_data():
             file.seek(0)
             # convert back to json.
             json.dump(file_data, file, indent = 4)
-
-        '''
-        json_url = os.path.join(app.static_folder,"","nobel.json")
-        data_json = json.load(open(json_url))
-        prizes_data = data_json['prizes']
-        updated_data = prizes_data.append(data)
-        '''
-        return data
-        #return redirect(url_for("save_data", data))
-        
-        '''(yr=year, cat = category, \
-            num_id=n_id, fn=firstname, sn=surname, mot=motivation, shr=share)))
-        '''
+        # I'd like to return a statement that it was added and the data
+        # but I don't know how do that
+        return '<p>Data added </p>'
 
     else:    
         return render_template("add_data.html")
-
-@app.route("/save_data")
-def data(yr, cat, num_id, fn, sn, mot, shr):
-    print(yr, cat)
 
 if __name__ == "__main__":
     app.run(debug=True)
